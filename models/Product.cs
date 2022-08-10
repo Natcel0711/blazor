4913ea2b9b7207a184c1e5ac1dacfe8b1de67c30
Natce <natcelnieves@gmail.com>
2022-08-10 12:04:58 -0400

    Blazor CRUD

---
+++ models/Product.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace blazor.models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+    }
+}
